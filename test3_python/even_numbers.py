1be3b4280f7501da7c1e18972bf616acbb2b567f
RamziHkiri <6257@holbertonstudents.com>
2023-09-04 14:57:11 +0200

    even_numbers.py

--- test3_python/even_numbers.py
+++ test3_python/even_numbers.py
@@ -7,3 +7,6 @@ def  filter_even_numbers(input_list):
             new_list.append(input_list[i])
     return new_list
 
+input_list = [3, 8, 12, 7, 4, 6, 9, 10]
+even_numbers = filter_even_numbers(input_list)
+print(even_numbers) 
